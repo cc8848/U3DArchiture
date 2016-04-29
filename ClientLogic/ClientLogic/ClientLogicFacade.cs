741f5ef3d9c0fde2a99f5dff7ce283b1d6bb6cd7
wmm <469486139@qq.com>
2016-04-29 17:33:31 +0800

    dll工程

---
+++ ClientLogic/ClientLogic/ClientLogicFacade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientLogic
+{
+    public class ClientLogicFacade
+    {
+		public int TestAdd(int a)
+		{
+			return a + 10;
+		}
+    }
+}
